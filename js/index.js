b9e9f6c889feda47696f0f672f59582b63c886ae
diugalde <dua.0695@gmail.com>
2015-04-10 20:10:59 -0600

    JSON input presentation finished
    Homepage with the text and file inputs finished with their respective styles.
    JSON input parse to the graph format finished

--- js/index.js
+++ js/index.js
@@ -1,39 +1,115 @@
-
 $(document).ready(function() {
 
+    /*Global variables*/
 
     $('#tabs').tab();
-    
-    
-    function loadFile() {
-        var input, file, fr;
 
-        if (typeof window.FileReader !== 'function') {
-            alert("The file API isn't supported on this browser yet.");
+    $("#json-input").change(function() {
+        $(".json-alert2").hide(); 
+        $('#input-file-button').css('display','block');
+    });
+
+
+
+    /*When the submit button is clicked to send the Text JSON input*/
+    $('#input-text-button').click(function() {
+        var jsonInput = $("#text-json").val();
+        if (jsonInput != '' && validJSON(jsonInput)) {
+            var jsonObject = JSON.parse(jsonInput);
+            convertJSON(jsonObject);
+        }else{
+            $(".json-alert1").show();
+        }
+    });
+
+    /*When the upload button is clicked*/
+    $('#input-file-button').click(function() {
+        var file = $("#json-input")[0].files[0];
+        var jsonInput = readFile(file);
+        console.log(jsonInput);
+    });
+
+    
+    function readFile(file) {
+        if(file.name.split('.').pop() != 'json') {
+            $(".json-alert2").show();   
             return;
         }
+        var reader = new FileReader();
+        var deferred = $.Deferred();
 
-        input = document.getElementById('fileinput');
-        if (!input) {
-            alert("Um, couldn't find the fileinput element.");
-        }
-        else if (!input.files) {
-            alert("This browser doesn't seem to support the `files` property of file inputs.");
-        }
-        else if (!input.files[0]) {
-            alert("Please select a file before clicking 'Load'");
-        }
-        else {
-            file = input.files[0];
-            fr = new FileReader();
-            fr.onload = receivedText;
-            fr.readAsText(file);
-        }
+        reader.onloadend = function(event) {
+            deferred.resolve(event.target.result);
+        };
 
-        function receivedText(e) {
-            lines = e.target.result;
-            var newArr = JSON.parse(lines); 
+        reader.onerror = function() {
+            deferred.reject(this);
+        };
+
+        reader.readAsText(file);
+
+        return deferred.promise();
+    }
+
+
+    function checkJSON(jsonString) {
+        if(validJSON(jsonString)) {
+            console.log("jola");
+            jsonObject = JSON.parse(jsonString);
+        }else{
+            $(".json-alert2").show();
         }
+    }
+
+    function validJSON(jsonInput) {
+        return (/^[\],:{}\s]*$/.test(jsonInput.replace(/\\["\\\/bfnrtu]/g, '@').replace(/"[^"\\\n\r]*"|true|false|null|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?/g, ']').replace(/(?:^|:|,)(?:\s*\[)+/g, '')));
+    }
+
+    /*Alerts close button behavior*/
+
+    $("#close1").click(function() {
+        $(".json-alert1").hide(); 
+    });
+
+    $("#close2").click(function() {
+        $(".json-alert2").hide(); 
+    });
+    
+    function convertJSON(jsonObject) {
+        var graphLength = Object.keys(jsonObject).length;
+        var newGraph = {};
+        var newNodes = [];
+        var newLinks = [];
+        
+        newGraph["directed"] = "true";
+        newGraph["multigraph"] = "false";
+        newGraph["graph"] = [];
+        
+        for(var i = 1; i <= graphLength; i++) {
+            var currNode = jsonObject["node"+i];
+            var newNode = {};
+            newNode["id"] = currNode["name"];
+            newNode["description"] = currNode["description"];
+            newNode["link"] = currNode["link"];
+            newNodes.push(newNode);
+            
+            var currNodeLinks = currNode["connectsTo"];
+            for(var j = 0; j < currNodeLinks.length; j++) {
+                var link = {};
+                var source = i-1;
+                var target = getNodeNumber(currNodeLinks[j])-1;
+                link["source"] = source;
+                link["target"] = target;
+                newLinks.push(link);
+            }
+        }
+        newGraph["links"] = newLinks;
+        newGraph["nodes"] = newNodes;
+    }
+    
+    
+    function getNodeNumber(nodeName) {
+        return parseInt(nodeName.substr(4));
     }
 
 });
